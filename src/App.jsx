715e937713ab6fc38b9b013d5f7e5826cc7fdae4
Farhodnazarov <nazarovfarhod0022@gmail.com>
2025-07-26 10:46:58 +0500

    nimadir

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import { FaFileDownload } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="bg-black  text-white min-h-screen pb-40">
+      <div className=" container mx-auto">
+        <div className="sticky top-0 z-20">
+          <Navbar />
+        </div>
+        <div id="home" className="text-center mb-5 scroll-mt-28">
+          <Header />
+        </div>
+        <div className="w-full text-center">
+          <h1 className="text-3xl tracking-[4px] mb-10 font-bold md:text-4xl md:mb-16 lg:text-5xl lg:mb-20">
+            Frontend Developer
+          </h1>
+        </div>
+        <div
+          id="about"
+          className="text-center scroll-mt-28  mb-10 md:mb-16 lg:mb-20"
+        >
+          <About />
+        </div>
+        <div className="w-full flex justify-center mb-10 lg:mb-20">
+          <a
+            className=" flex items-center gap-3 font-bold py-4 px-8 rounded-3xl bg-[#222] hover:bg-white hover:text-[#222] transition-colors
+              md:text-3xl md:w-[400px] lg:w-[450px] lg:text-4xl"
+            href="./rezyume.pdf"
+            download
+          >
+            Download Rezyume <FaFileDownload />
+          </a>
+        </div>
+        <div id="projects" className="text-center scroll-mt-28">
+          <Projects />
+        </div>
+        <div id="skills" className="text-center scroll-mt-28">
+          <Skills />
+        </div>
+        <div id="contact" className="text-center scroll-mt-28">
+          <Contact />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,221 @@
+import { IoLogoVercel } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
+
+function Projects() {
+  return (
+    <div className="px-10 mb-20">
+      <h2 className="text-4xl font-bold mb-5 lg:mb-20 tracking-[4px] lg:text-5xl">
+        My Projects
+      </h2>
+      <ul className="flex flex-wrap gap-5 justify-center">
+        <li className="flex flex-col items-center gap-5 shadow-white w-80 py-8 bg-[#222] rounded-xl">
+          <h3 className="text-xl font-bold md:text-2xl lg:text-3xl">
+            Tip Calculator
+          </h3>
+          <div className="flex gap-8">
+            <a
+              href="https://github.com/Nazarovfarhod/Calculator-of-tip.git"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <FaGithub className="w-8 h-8" />
+              <span className="">GitHub</span>
+            </a>
+            <a
+              href="https://calculator-of-tip.vercel.app/"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <IoLogoVercel className="w-8 h-8" />
+              <span className="">Vercel</span>
+            </a>
+          </div>
+        </li>
+        <li className="flex flex-col items-center gap-5 shadow-white w-80 py-8 bg-[#222] rounded-xl">
+          <h3 className="text-xl font-bold md:text-2xl lg:text-3xl">
+            Devfinder
+          </h3>
+          <div className="flex gap-8">
+            <a
+              href="https://github.com/Nazarovfarhod/devfinder.git"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <FaGithub className="w-8 h-8" />
+              <span className="">GitHub</span>
+            </a>
+            <a
+              href="https://devfinder-pink-pi.vercel.app/"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <IoLogoVercel className="w-8 h-8" />
+              <span className="">Vercel</span>
+            </a>
+          </div>
+        </li>
+        <li className="flex flex-col items-center gap-5 shadow-white w-80 py-8 bg-[#222] rounded-xl">
+          <h3 className="text-xl font-bold md:text-2xl lg:text-3xl">
+            FN Furniture
+          </h3>
+          <div className="flex gap-8">
+            <a
+              href="https://github.com/Nazarovfarhod/Furniture.git"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <FaGithub className="w-8 h-8" />
+              <span className="">GitHub</span>
+            </a>
+            <a
+              href="https://furniture-five-ashy.vercel.app/"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <IoLogoVercel className="w-8 h-8" />
+              <span className="">Vercel</span>
+            </a>
+          </div>
+        </li>
+        <li className="flex flex-col items-center gap-5 shadow-white w-80 py-8 bg-[#222] rounded-xl">
+          <h3 className="text-xl font-bold md:text-2xl lg:text-3xl">Quiz</h3>
+          <div className="flex gap-8">
+            <a
+              href="https://github.com/Nazarovfarhod/Quiz.git"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <FaGithub className="w-8 h-8" />
+              <span className="">GitHub</span>
+            </a>
+            <a
+              href="https://quiz-dusky-psi.vercel.app/"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <IoLogoVercel className="w-8 h-8" />
+              <span className="">Vercel</span>
+            </a>
+          </div>
+        </li>
+        <li className="flex flex-col items-center gap-5 shadow-white w-80 py-8 bg-[#222] rounded-xl">
+          <h3 className="text-xl font-bold md:text-2xl lg:text-3xl">
+            My Kitchen
+          </h3>
+          <div className="flex gap-8">
+            <a
+              href="https://github.com/Nazarovfarhod/My-kitchen.git"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <FaGithub className="w-8 h-8" />
+              <span className="">GitHub</span>
+            </a>
+            <a
+              href="https://my-kitchen-five.vercel.app/"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <IoLogoVercel className="w-8 h-8" />
+              <span className="">Vercel</span>
+            </a>
+          </div>
+        </li>
+        <li className="flex flex-col items-center gap-5 shadow-white w-80 py-8 bg-[#222] rounded-xl">
+          <h3 className="text-xl font-bold md:text-2xl lg:text-3xl">
+            Products shop
+          </h3>
+          <div className="flex gap-8">
+            <a
+              href="https://github.com/Nazarovfarhod/Login-Logout.git"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <FaGithub className="w-8 h-8" />
+              <span className="">GitHub</span>
+            </a>
+            <a
+              href="https://login-logout-psi.vercel.app/"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <IoLogoVercel className="w-8 h-8" />
+              <span className="">Vercel</span>
+            </a>
+          </div>
+        </li>
+        <li className="flex flex-col items-center gap-5 shadow-white w-80 py-8 bg-[#222] rounded-xl">
+          <h3 className="text-xl font-bold md:text-2xl lg:text-3xl">
+            Gues my number
+          </h3>
+          <div className="flex gap-8">
+            <a
+              href="https://github.com/Nazarovfarhod/GussMyNumber.git"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <FaGithub className="w-8 h-8" />
+              <span className="">GitHub</span>
+            </a>
+            <a
+              href="https://guss-my-number-u7i1.vercel.app/"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <IoLogoVercel className="w-8 h-8" />
+              <span className="">Vercel</span>
+            </a>
+          </div>
+        </li>
+        <li className="flex flex-col items-center gap-5 shadow-white w-80 py-8 bg-[#222] rounded-xl">
+          <h3 className="text-xl font-bold md:text-2xl lg:text-3xl">
+            Audio player.
+          </h3>
+          <div className="flex gap-8">
+            <a
+              href="https://github.com/Nazarovfarhod/Musics-Player.git"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <FaGithub className="w-8 h-8" />
+              <span className="">GitHub</span>
+            </a>
+            <a
+              href="https://musics-player-two.vercel.app"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <IoLogoVercel className="w-8 h-8" />
+              <span className="">Vercel</span>
+            </a>
+          </div>
+        </li>
+        <li className="flex flex-col items-center gap-5 shadow-white w-80 py-8 bg-[#222] rounded-xl">
+          <h3 className="text-xl font-bold md:text-2xl lg:text-3xl">
+            Random User
+          </h3>
+          <div className="flex gap-8">
+            <a
+              href="https://github.com/Nazarovfarhod/random-user.git"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <FaGithub className="w-8 h-8" />
+              <span className="">GitHub</span>
+            </a>
+            <a
+              href="https://random-user-ashen-chi.vercel.app/"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <IoLogoVercel className="w-8 h-8" />
+              <span className="">Vercel</span>
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
