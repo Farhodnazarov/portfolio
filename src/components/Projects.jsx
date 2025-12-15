b6184bc6e3594a3f76ec609e31730507aa9b0a41
Farhodnazarov <nazarovfarhod0022@gmail.com>
2025-12-15 18:19:41 +0500

    added new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -228,6 +228,29 @@ function Projects() {
             </a>
           </div>
         </li>
+        <li className="flex flex-col items-center gap-5 shadow-white w-80 py-8 bg-[#222] rounded-xl">
+          <h3 className="text-xl font-bold md:text-2xl lg:text-3xl">
+            My Todo App
+          </h3>
+          <div className="flex gap-8">
+            <a
+              href="https://github.com/Farhodnazarov/myTodoList"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <FaGithub className="w-8 h-8" />
+              <span className="">GitHub</span>
+            </a>
+            <a
+              href="https://my-todo-list-iota-black.vercel.app/login"
+              className="flex items-center gap-5 hover:text-[#0079FF] hover:underline transition-colors"
+              target="_blank"
+            >
+              <IoLogoVercel className="w-8 h-8" />
+              <span className="">Vercel</span>
+            </a>
+          </div>
+        </li>
       </ul>
     </div>
   );
